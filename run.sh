52d3d124fd437ea4e03a56b8ef54a46a7e379194
marceloalmeidaleao <39470750+marceloalmeidaleao@users.noreply.github.com>
2020-05-05 18:44:11 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,16 @@
 while :; do
 sleep 300
 done
+
+cd /kafka/bin
+nohup ./zookeeper-server-start.sh ../config/zookeeper.properties > dev/null 2>&1
+
+while :; do
+sleep 600
+done
+
+nohup ./kafka-server-start.sh ../config/server.properties > dev/null 2>&1
+echo -e "started"
+
+
+
