a8a24de7a1041cadde0834081e68e064ff760863
marceloalmeidaleao <39470750+marceloalmeidaleao@users.noreply.github.com>
2020-05-30 01:29:17 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@ cd /kafka/
 chmod -R 775 *
 
 cd /kafka/bin
-nohup ./zookeeper-server-start.sh ../config/zookeeper.properties &
+nohup ./zookeeper-server-start.sh ../config/zookeeper.properties > dev/null 2>&1
 
 while :; do
 sleep 600
 done
 
-./kafka-server-start.sh ../config/server.properties &
+./kafka-server-start.sh ../config/server.properties & dev/null 2>&1
 echo -e "started"
 
 
